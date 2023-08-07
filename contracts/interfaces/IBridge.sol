f0a70e63350d7f3a32a510ea2c71a1eb847ee5cb
Confucian <confucian.e@gmail.com>
2023-08-07 13:53:44 +0800

    chore: Refactor code formatting

--- contracts/interfaces/IBridge.sol
+++ contracts/interfaces/IBridge.sol
@@ -2,9 +2,50 @@
 pragma solidity ^0.8.16;
 
 interface IBridge {
+    // ===================================================== Errors =====================================================
+
     error SameToken();
     error PoolExists();
 
+    // ===================================================== Events =====================================================
+
     event BridgeTransfer(address indexed token, uint256 amount);
-    event BridgeSwap(address indexed fromToken, address indexed toToken, uint256 amount);
+    event BridgeSwap(
+        address indexed fromToken,
+        address indexed toToken,
+        uint256 amount
+    );
+
+    // ===================================================== Read Functions =====================================================
+
+    function poolTotalAmount() external view returns (uint256 totalAmount);
+
+    function getPoolInfo(
+        address tokenA,
+        address tokenB
+    )
+        external
+        view
+        returns (
+            address token0,
+            address token1,
+            uint256 amount0,
+            uint256 amount1
+        );
+
+    // ===================================================== Write Functions =====================================================
+
+    function initPool(
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB
+    ) external returns (uint256 share);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB
+    ) external returns (uint256 share);
 }
