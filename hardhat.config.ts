d7aa90d92208c20dc4374d217f54385ac996989f
Confucian <confucian.e@gmail.com>
2023-08-07 20:51:45 +0800

    feat: support script & verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.Sepolia_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.Mumbai_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.EtherScan_API_KEY!,
+      polygonMumbai: process.env.PolygonScan_API_KEY!,
+    }
+  }
 };
 
 export default config;
